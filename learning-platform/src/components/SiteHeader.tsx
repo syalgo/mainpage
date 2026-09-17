bb60b176381cbce08b4bd4be7e0cd3a70567a4fd
syalgo <ljdeok2@gmail.com>
2026-09-17 13:44:55 +0900

    Simplify learning platform header

--- learning-platform/src/components/SiteHeader.tsx
+++ learning-platform/src/components/SiteHeader.tsx
@@ -13,10 +13,6 @@ export default async function SiteHeader() {
           <span><strong>세영 학습 플랫폼</strong><small>SEYOUNG ONLINE CLASSROOM</small></span>
         </Link>
         <nav className="nav-links">
-          <Link href="/materials">기본 교재</Link>
-          <Link href="/specialized">특성화고</Link>
-          <Link href="/koi">정보올림피아드</Link>
-          {user?.admin && <Link href="/admin">관리자</Link>}
           {user ? (
             <span className="header-account">
               <Link href="/account">내 계정</Link>
